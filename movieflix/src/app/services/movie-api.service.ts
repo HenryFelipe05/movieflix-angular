3f8c759d249e40683d7aa42df682efa715246fdf
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-05-19 22:31:25 -0300

    feat: adicionado carrosel e consumindo dados da API.

--- movieflix/src/app/services/movie-api.service.ts
+++ movieflix/src/app/services/movie-api.service.ts
@@ -9,6 +9,16 @@ export class MovieApiService {
 
   constructor(private http: HttpClient) { }
 
-  
+  baseUrl = 'https://api.themoviedb.org/3'
+  options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMWNlZjdkNjQ1YTY3YzAxNGY5MzA0NDNiN2QyMjk3ZSIsIm5iZiI6MTc0NzY5OTcyMS4xMiwic3ViIjoiNjgyYmM4MDkyOTYxMDBmZmRlNjQ5MDQxIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.50fJlUpOwcXGzus_UWd_oV0LvwlDkPfkTF3P6IBSFf0'
+    }
+  };
 
+  bannerApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
+  }
 }
