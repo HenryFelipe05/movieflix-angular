1310b28be7b8f10baf775ca62d19b1fa8b5abb8c
Henry Felipe <henry.oliveira11@etec.sp.gov.br>
2025-05-26 22:24:35 -0300

    feat: Adicionando seções de filmes e series.

--- movieflix/src/app/pages/home/home.component.ts
+++ movieflix/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MovieApiService } from '../../services/movie-api.service';
+import { AutoScrollDirective } from '../../directives/auto-scroll.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AutoScrollDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -13,14 +14,38 @@ export class HomeComponent {
   constructor(private service: MovieApiService) { }
 
   bannerResults: any = [];
+  trendingMovieResults: any = [];
+  trendingSerieResults: any = [];
+  popularActionMovieResults: any = [];
 
   ngOnInit(): void {
     this.bannerData();
+    this.trendingMovieData();
+    this.trendingSerieData();
+    this.popularActionMovieData();
   }
 
   bannerData() {
     this.service.bannerApiData().subscribe((result) => {
       this.bannerResults = result.results;
+    });
+  }
+
+  trendingMovieData() {
+    this.service.trendingMovieApiData().subscribe((result) => {
+      this.trendingMovieResults = result.results;
+    });
+  }
+
+  trendingSerieData() {
+    this.service.trendingSerieApiData().subscribe((result) => {
+      this.trendingSerieResults = result.results;
+    });
+  }
+
+  popularActionMovieData() {
+    this.service.popularActionMovieApiData().subscribe((result) => {
+      this.popularActionMovieResults = result.results;
     });
   }
 }
